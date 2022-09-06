3e2829fb3100bd22340b42049434711825f68211
Raúl Ibañez Jorge <rauljorge1919@gmail.com>
2022-09-06 15:36:20 +0200

    006/09/22_02_(List_films_finished)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'list_of_films';
+
+  url_one : string = "https://api.themoviedb.org/3/search/movie?api_key=";
+  token : string = "b09cf43a0ed2b21773ecc1e9f71d4a90";
+  url_two : string = "&language=en-US&query=";
+  query ?: string;
+  url_three : string = "&page=1&include_adult=false"
+
+  filmArray ?: any;
+
+  constructor(){  }
+
+  public searchFilms(event: Event) {
+    let input = (<HTMLInputElement>document.getElementById("search")).value;
+    this.searchTitle(input);
+  }
+
+  public async searchTitle(input: string){
+    let res = await fetch(this.url_one + this.token + this.url_two + input + this.url_three)
+    let parsed = await res.json();
+    this.getFilms(parsed.results);
+  }
+
+  public getFilms(json: any){
+    this.filmArray = [];
+    json.forEach((element: any) => {
+      this.filmArray?.push(element);
+      console.log(element.title);
+    });
+    console.log(this.filmArray);
+  }
+
+  public clickedFilm(event: Event){
+    console.log(event.target);
+  }
 }
